696d84312c2264c1cc76f5419d403743ddb2ed2b
Felix Anducho <felix.anducho74@gmail.com>
2023-05-18 20:57:57 -0600

    add test for close method

--- internal/log/store_test.go
+++ internal/log/store_test.go
@@ -68,3 +68,30 @@ func testReadAt(t *testing.T, s *store) {
 		off += int64(n)
 	}
 }
+
+func TestStoreClose(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_close_test")
+	require.NoError(t, err)
+
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+	_, _, err = s.Append(write)
+	require.NoError(t, err)
+
+	f, beforeSize, err := openFile(f.Name())
+	require.NoError(t, err)
+
+	err = s.Close()
+	require.NoError(t, err)
+
+	_, afterSize, err := openFile(f.Name())
+	require.NoError(t, err)
+	require.True(t, afterSize > beforeSize)
+
+}
+
+func openFile(s string) {
+	panic("unimplemented")
+}
